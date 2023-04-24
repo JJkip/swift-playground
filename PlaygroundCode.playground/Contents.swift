659acd0f52b49ce94053138a3b7a17240587d32e
jjkip <jjlangatjose@gmail.com>
2023-04-24 15:15:09 +0300

    working with type inference

---
+++ PlaygroundCode.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting1 = "Hello, playground 1"
+var greetings2: String = "Hello, playground 2"
+
+let constant1 = 10
+let constant2:Int = 10
+let double1 = 20.5
+let double2:Double = 40.7
+
+let result1 = constant1 + constant2
+var result2:Int = constant1 + result1
+
+var result3 = double1 + double2
