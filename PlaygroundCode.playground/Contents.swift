90aae12c8a96361daecd940f7089fe9f83d3dade
jjkip <jjlangatjose@gmail.com>
2023-04-24 15:32:23 +0300

    working with text

--- PlaygroundCode.playground/Contents.swift
+++ PlaygroundCode.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//Type inference
 var greeting1 = "Hello, playground 1"
 var greetings2: String = "Hello, playground 2"
 
@@ -12,3 +13,22 @@ let result1 = constant1 + constant2
 var result2:Int = constant1 + result1
 
 var result3 = double1 + double2
+
+//Working with text
+var message = "You have received a new message"
+var greetings = "Hallo!"
+var client_name = "Chemaluch"
+
+var new_message = greetings + client_name + ", " + message
+
+new_message.uppercased()
+new_message.lowercased()
+new_message.count
+
+var bookPrice = 84
+var numOfCopies = 4
+var totalPrice = bookPrice * numOfCopies
+var totalPriceMessage1 = "The price of the book is $" + String(totalPrice)
+//String Interpolations
+var totalPriceMessage2 = "The price of the book is $\(totalPrice)"
+
